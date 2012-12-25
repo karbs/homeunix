0939814313c4d6d70382465ae4d73386b8f0a441
d.travnikov@gmail.com <d.travnikov@gmail.com@fc8433b4-f986-8c03-71af-1d9c613bc296>
2012-12-25 07:27:32 +0000

    cpuload added
    git-svn-id: https://homeunix.googlecode.com/svn/trunk@130 fc8433b4-f986-8c03-71af-1d9c613bc296

---
+++ src/cpuload/cpuload.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <time.h>
+#define N 10000
+main() {
+	int i, j, result;
+	clock_t t0 = clock();
+	result = 0;
+	for (i = 0; i < N; ++i)
+		for (j = 0; j < i; ++j)
+			result = (result % 456) + (i % (j + 1)) + (j % 567);
+	clock_t t = clock() - t0;
+	printf("result: %ld, clock: %ld\n", result, t);
+}
